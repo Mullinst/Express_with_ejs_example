4e3b487fa97ec21192ac27459fcb383ea1bdade1
Mullinst <mullinst2011@gmail.com>
2019-11-23 01:21:14 -0500

    Updated database init scirpt.

--- model/db-init.js
+++ model/db-init.js
@@ -1,17 +1,17 @@
-var sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
 
 // Define database file path
-var filePath = "IronMeteorite.db"
+const filePath = "IronMeteorite.db"
 
 // Init/connect to DB
-var db = new sqlite3.Database(filePath, (error) => {
-				// Callback function 
-				if(error !== null){
-					console.log("Error connecting to database.");
-				} else {
-					console.log("Connected to Iron Meteorite Database.");
-				}
-			});
+const db = new sqlite3.Database(filePath, (error) => {
+	// Callback function 
+	if(error !== null){
+		console.log("Error connecting to database.");
+	} else {
+		console.log("Connected to Iron Meteorite Database.");
+	}
+});
 
 // User Table SQL
 var sql = " CREATE TABLE IF NOT EXISTS users(" +
@@ -20,19 +20,27 @@ var sql = " CREATE TABLE IF NOT EXISTS users(" +
 				"password TEXT)";
 
 // Run query
-db.run(sql);
+db.run(sql, (err) => {
+	console.log(err);
+});
 
 
 // Inserting mock data
 db.run("INSERT INTO users (name, password) VALUES ($name, $password)", {
-          $name: "Fred",
-          $password: "Butterbeer16"
-      });
+	$name: "Fred",
+    $password: "Butterbeer16"
+}, (err) => {
+	console.log(err);
+});
 
 db.run("INSERT INTO users (name, password) VALUES ($name, $password)", {
-          $name: "George",
-          $password: "Nimbus2018"
-      });
+	$name: "George",
+	$password: "Nimbus2018"
+}, (err) => {
+	console.log(err);
+});
 
 // Closing DB connection
-db.close();
+db.close((err) => {
+	console.log('Failed to close database connection.');
+});
